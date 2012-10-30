e0aad5906da59a63ab9ba914e7be7c588efb96a6
Kenny Do <kennydo@berkeley.edu>
2012-10-30 13:45:42 -0700

    the authn window now saves to credential store

--- airbears_supplicant_gtk/ui/calnetauthn.py
+++ airbears_supplicant_gtk/ui/calnetauthn.py
@@ -1,31 +1,54 @@
+import logging
+import os
+
+from airbears_supplicant_gtk import log
 from gi.repository import Gtk
 
+logger = logging.getLogger(__name__)
+
 class CalNetAuthnDialog:
-    def __init__(self):
+    def __init__(self, credential_store):
+        self.credential_store = credential_store
+
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("glade/calnetauthn.glade")
+        self.builder.add_from_file(self.glade_file_path())
 
         handlers = {
             "on_window_destroy": Gtk.main_quit,
             "on_clear_button_pressed": self.on_clear_button_pressed,
-
+            "on_save_button_pressed": self.on_save_button_pressed,
         }
         self.builder.connect_signals(handlers)
+
+        self.username_entry = self.builder.get_object("username_entry")
+        self.password_entry = self.builder.get_object("password_entry")
+
+    def glade_file_path(self):
+        return os.path.join(os.path.dirname(__file__), 'glade/calnetauthn.glade')
 
     def show(self):
         window = self.builder.get_object("window")
         window.show_all()
 
     def on_clear_button_pressed(self, button):
-        username_entry = self.builder.get_object("username_entry")
-        password_entry = self.builder.get_object("password_entry")
+        logger.debug("Pressed the Clear button")
+        self.username_entry.set_text("")
+        self.password_entry.set_text("")
 
-        username_entry.set_text("")
-        password_entry.set_text("")
+    def on_save_button_pressed(self, button):
+        username = self.username_entry.get_text()
+        password = self.password_entry.get_text()
 
+        logger.debug("Save buton pressed with username %s" % username)
+
+        self.credential_store.save(username, password)
 
 if __name__ == "__main__":
-    test_window = CalNetAuthnDialog()
+    from airbears_supplicant_gtk.calnet.credentialstore import TestCalNetCredentialStore
+
+    cred_store = TestCalNetCredentialStore()
+
+    test_window = CalNetAuthnDialog(cred_store)
     test_window.show()
 
     Gtk.main()

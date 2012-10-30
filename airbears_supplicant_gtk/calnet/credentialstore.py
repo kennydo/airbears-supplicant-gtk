e8d0184825f0e08135f04c6ff0093c7aa0e93a34
Kenny Do <kennydo@berkeley.edu>
2012-10-30 13:46:37 -0700

    created calnet credential store class

---
+++ airbears_supplicant_gtk/calnet/credentialstore.py
@@ -0,0 +1,32 @@
+import logging
+
+from airbears_supplicant_gtk import log
+
+logger = logging.getLogger(__name__)
+
+class CalNetCredentialStore:
+    def save(self, username, password):
+        """
+        Save the username and password.
+        
+        Return True if successful, else return False.
+        """
+        raise NotImplementedError()
+
+    def has_credentials(self):
+        """
+        Return True if the store has credentials saved, else False
+        """
+        raise NotImplementedError()
+
+class TestCalNetCredentialStore(CalNetCredentialStore):
+    def __init__(self):
+        self.has_creds = False
+
+    def save(self, username, password):
+        logger.debug("Called save with username: %s" % username)
+        self.has_creds = True
+        return True
+
+    def has_credentials(self):
+        return self.has_creds

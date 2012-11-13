918fc0b0f3925afb861de00550be5647dfbb7121
Kenny Do <kennydo@berkeley.edu>
2012-11-12 17:59:23 -0800

    added debug logging

--- airbears_supplicant_gtk/calnet/credentialstore.py
+++ airbears_supplicant_gtk/calnet/credentialstore.py
@@ -7,10 +7,6 @@ from gi.repository import GnomeKeyring
 
 logger = logging.getLogger(__name__)
 
-
-KEYRING_NAME = 'login'
-
-
 class CredentialStore:
     def save(self, username, password):
         """
@@ -57,32 +53,42 @@ class GnomeCredentialStore(CredentialStore):
         GnomeKeyring.Attribute.list_append_string(self.item_attrs, 'id', GnomeCredentialStore.ITEM_ATTR_ID)
         
     def _encode_credentials(self, username, password):
+        logger.debug("Encoding credentials")
         return "%s@@@%s" % (base64.b64encode(username),
                             base64.b64encode(password))
 
     def _decode_credentials(self, encoded):
+        logger.debug("Attempting to decode credentials")
         if "@@@" in encoded:
             split = encoded.split("@@@")
             try:
                 username = base64.b64decode(split[0])
                 password = base64.b64decode(split[1])
             except:
+                logger.deubg("Error base64 decoding credentials")
                 return None
+            logger.debug("Successfully retrieved credentials for username: %s" % username)
             return (username, password)
+        logger.debug("Credentials were stored in a strange format")
         return None
         
     def _find_keyring_item_id(self):
         cached_value = getattr(self, '_cached_item_id', None)
         if cached_value:
             # only 1 keyring item is needed, so it's safe to cache
+            logger.deubg("Retrieved cached keyring item id from cache: %s" % cached_value)
             return self._cached_item_id
-
+            
+        logger.debug("No keyring item id cached, so need to create the item")
         result, value = GnomeKeyring.find_items_sync(
                             GnomeKeyring.ItemType.GENERIC_SECRET,
                             self.item_attrs)
         if result != GnomeKeyring.Result.OK:
+            logger.debug("There was an error finding the keyring item: %s" % result)
             return None
+        
         self._cached_item_id = value[0].item_id
+        logger.debug("Successfully found keyring item id: %s" % self._cached_item_id)
         return self._cached_item_id
      
     def _get_keyring_item_info(self, item_id):
@@ -90,7 +96,9 @@ class GnomeCredentialStore(CredentialStore):
                             GnomeCredentialStore.KEYRING_NAME,
                             item_id)
         if result != GnomeKeyring.Result.OK:
+            logger.debug("Failed to get keyring info for item_id %s: %s" % (item_id, result))
             return None
+        logger.debug("Successfully got keyring item info for item_id %s" % item_id)
         return value
         
     def _create_keyring_item_info(self, secret):
@@ -102,7 +110,9 @@ class GnomeCredentialStore(CredentialStore):
                                 secret,
                                 True)
         if result != GnomeKeyring.Result.OK:
-           return None
+            logger.debug("Failed to create keyring item: %s" % result)
+            return None
+        logger.debug("Successfully created keyring item info with item id: %s " % item_id)
         return item_id
     
     def save(self, username, password):
@@ -118,6 +128,7 @@ class GnomeCredentialStore(CredentialStore):
                 GnomeCredentialStore.KEYRING_NAME,
                 item_id,
                 item_info)
+        logger.debug("Successfully saved new credentials to item_id: %s" % item_id)
         return True
         
     def get_credentials(self):

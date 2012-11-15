cf7b48e70987de20125f6b74042d0233eaa5e312
Kenny Do <kennydo@berkeley.edu>
2012-11-14 20:23:46 -0800

    dialog window emits save-failed signal when save fails

--- airbears_supplicant_gtk/ui/calnetauthn.py
+++ airbears_supplicant_gtk/ui/calnetauthn.py
@@ -2,12 +2,25 @@ import logging
 import os
 
 from airbears_supplicant_gtk import log
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
 logger = logging.getLogger(__name__)
 
-class CalNetAuthnDialog:
+class CalNetAuthnDialog(GObject.GObject):
+    __gsignals__ = {
+        'dialog-saved': (GObject.SIGNAL_RUN_FIRST,
+                         None,
+                         ()),
+        'dialog-cleared': (GObject.SIGNAL_RUN_FIRST,
+                           None,
+                           ()),
+        'save-failed': (GObject.SIGNAL_RUN_FIRST,
+                        None,
+                        (str,)),
+    }
+
     def __init__(self, credential_store):
+        super(CalNetAuthnDialog, self).__init__()
         self.credential_store = credential_store
 
         self.builder = Gtk.Builder()
@@ -22,6 +35,11 @@ class CalNetAuthnDialog:
 
         self.username_entry = self.builder.get_object("username_entry")
         self.password_entry = self.builder.get_object("password_entry")
+
+        if self.credential_store.has_credentials():
+            (username, password) = self.credential_store.get_credentials()
+            self.username_entry.set_text(username)
+            self.password_entry.set_text(password)
 
     def glade_file_path(self):
         return os.path.join(os.path.dirname(__file__), 'assets', 'calnetauthn.glade')
@@ -40,6 +58,13 @@ class CalNetAuthnDialog:
         password = self.password_entry.get_text()
 
         logger.debug("Save buton pressed with username %s" % username)
+
+        if not username:
+            self.emit('save-failed', 'Unable to save blank username')
+            return
+        if not password:
+            self.emit('save-failed', 'Unable to save blank password')
+            return
 
         self.credential_store.save(username, password)
         self.on_window_delete_event()
